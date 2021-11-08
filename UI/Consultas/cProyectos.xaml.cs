4fcb9aed49964ee14cafe34a8cca58f941424994
Frankelyn <frankelyn_garcia@ucne.edu.do>
2021-11-08 19:46:15 -0400

    Agregando el codigo del buscar en la consulta de Proyectos

--- UI/Consultas/cProyectos.xaml.cs
+++ UI/Consultas/cProyectos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P2_AP1_Frankelyn_20190276.BLL;
+using P2_AP1_Frankelyn_20190276.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +28,56 @@ namespace P2_AP1_Frankelyn_20190276.UI.Consultas
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            List<Proyectos> listado = new List<Proyectos>();
 
+            if(CriterioTextbox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        {
+                            if (DesdeDatePicker.SelectedDate != null)
+                                listado = ProyectosBLL.GetList(
+                                    x => x.Fecha.Date >= DesdeDatePicker.SelectedDate && x.Fecha.Date <= HastaDatePicker.SelectedDate &&
+                                    x.ProyectoId == Utilidades.Toint(CriterioTextbox.Text));
+                            else
+                                listado = ProyectosBLL.GetList(x => x.ProyectoId == Utilidades.Toint(CriterioTextbox.Text));
+                            break;
+                        }
+
+                    case 1:
+                        {
+                            if (DesdeDatePicker.SelectedDate != null)
+                                listado = ProyectosBLL.GetList(
+                                    x => x.Fecha.Date >= DesdeDatePicker.SelectedDate && x.Fecha.Date <= HastaDatePicker.SelectedDate &&
+                                    x.Descripcion.ToLower().Contains(CriterioTextbox.Text.ToLower()));
+                            else
+                                listado = ProyectosBLL.GetList(x => x.Descripcion.ToLower().Contains(CriterioTextbox.Text.ToLower()));
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            if (DesdeDatePicker.SelectedDate != null)
+                                listado = ProyectosBLL.GetList(
+                                    x => x.Fecha.Date >= DesdeDatePicker.SelectedDate && x.Fecha.Date <= HastaDatePicker.SelectedDate &&
+                                    x.TiempoTotal == Utilidades.Toint(CriterioTextbox.Text));
+                            else
+                                listado = ProyectosBLL.GetList(x => x.TiempoTotal == Utilidades.Toint(CriterioTextbox.Text));
+                            break;
+                        }
+                }
+            }
+            else
+            {
+                listado = ProyectosBLL.GetList(x => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+
+            var conteo = listado.Count();
+            ConteoTextBox.Text = conteo.ToString();
         }
     }
 }
