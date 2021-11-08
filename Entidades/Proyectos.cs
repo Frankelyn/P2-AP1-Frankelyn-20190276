d1b19ea228ec23196f4ac24684504e972ad0249b
Frankelyn <frankelyn_garcia@ucne.edu.do>
2021-11-08 18:21:42 -0400

    Agregando entidad Proyectos

---
+++ Entidades/Proyectos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Frankelyn_20190276.Entidades
+{
+    public class Proyectos
+    {
+        [Key]
+        public int ProyectoId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        public string Descripcion { get; set; }
+
+        public int TiempoTotal { get; set; }
+
+       // [ForeignKey("ProyectoId")]
+        //public virtual List<ProyectoDetalle> Detalle { get; set; } = new List<ProyectoDetalle>(); 
+    }
+}
